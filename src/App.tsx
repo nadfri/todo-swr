63090803063825243ab2faf5cf817bcfdfc3144e
Nader Frigui <nadfri@gmail.com>
2024-08-27 21:12:29 +0200

    Add: Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Todos from './components/Todos/Todos';
 import Header from './components/Header/Header';
 import ErrorPage from './components/ErrorPage/ErrorPage';
 import { ErrorBoundary } from 'react-error-boundary';
 import NotFound from './components/ErrorPage/NotFound';
+import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
@@ -16,6 +18,7 @@ export default function App() {
             <Route path='*' element={<NotFound />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </ErrorBoundary>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <div className='Footer'>
+      <a href='https://github.com/nadfri/todo-swr'> NadfriJS @2024</a>
+    </div>
+  );
+}
