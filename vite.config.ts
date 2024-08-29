aad36a0d59e67ae8c4cdbffe98ee13e639873644
Nader Frigui <nadfri@gmail.com>
2024-08-29 23:10:43 +0200

    Delete: svgr doesnt works with vite swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
-import svgr from "vite-plugin-svgr";
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
